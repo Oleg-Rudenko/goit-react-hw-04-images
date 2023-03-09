c07bef8613c3ece27d0442ff7b9df992f9618d66
OLEH RUDENKO <51856760+GrazyMen@users.noreply.github.com>
2023-03-09 16:30:13 +0200

    hw

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,18 @@
+
+import { Vortex } from 'react-loader-spinner';
+import css from './Loader.module.css';
+
+export const Loader = () => (
+  <div className={css.Loader}>
+    <Vortex
+  
+  visible={true}
+  height="250"
+  width="250"
+  ariaLabel="vortex-loading"
+  wrapperStyle={{}}
+  wrapperClass="vortex-wrapper"
+  colors={['red', 'green', 'blue', 'yellow', 'orange', 'purple']}
+/>
+  </div>
+);
